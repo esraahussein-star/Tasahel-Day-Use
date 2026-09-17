da45953f9017a051d390922d5df84e09efc99639
Esraa Hussein <esraaessoo19@gmail.com>
2026-09-17 05:07:19 +0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  supabaseUrl: 'https://hgyhveuzhfjghqykzcxs.supabase.co',
+
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneWh2ZXV6aGZqZ2hxeWt6Y3hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NTg4NzQsImV4cCI6MjEwNDQzNDg3NH0.MbPJ7ayZqSnjcfLP_f-ZMRxuWNqgH6_ZPXFKFpGBVbM'
+};
